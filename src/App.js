f8210d632ba9e8f69fcbf8b558d2902af6cf3297
Arsalan Jawed <arsalanjaweed@gmail.com>
2020-11-23 03:12:24 +0500

    new react app'

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      hello from arsaalan this is my first react app
+    </div>
+  );
+}
+
+export default App;
